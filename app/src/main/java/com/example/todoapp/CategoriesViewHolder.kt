1e22c70457b3b8561e57be307326942ebcb7696f
Jose-Vela <joseeloyvo@gmail.com>
2023-06-08 23:14:37 -0600

    Refactorizamos nuevamente el código de CategoriesViewHolder.kt, esta vez quitando parte de código que se repetia dentro de la expresión condicional when(), dejando las dos lineas que controlan el texto de la card y el color del divider, al finalizar dicha expreción

--- app/src/main/java/com/example/todoapp/CategoriesViewHolder.kt
+++ app/src/main/java/com/example/todoapp/CategoriesViewHolder.kt
@@ -2,27 +2,43 @@ package com.example.todoapp
 
 import android.view.View
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class CategoriesViewHolder(view: View):  RecyclerView.ViewHolder(view){
+class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val tvCategoryName: TextView = view.findViewById(R.id.tvCategoryName)
     private val divider: View = view.findViewById(R.id.divider)
+    private val viewContainer: CardView = view.findViewById(R.id.viewContainer)
 
-    fun render(taskCategory: TaskCategory){
-        when(taskCategory){
+    fun render(taskCategory: TaskCategory, onItemSelected: (Int) -> Unit) {
+
+        val categoryText: Int   // Lo inicializamos como tipo entero, ya que la función getString() recibe por parametro un Int, (R.string.my_string = Int)
+        val categoryColor: Int  // Lo inicializamos como tipo entero, ya que la función getColor() recibe como segundo parametro un Int, que hace referencia a un color (R.color.my_color = Int)
+
+        val cardBackgroundColor = if (taskCategory.isSelected) R.color.todo_background_card else R.color.todo_background_disabled
+        viewContainer.setCardBackgroundColor(ContextCompat.getColor(viewContainer.context, cardBackgroundColor))
+
+        itemView.setOnClickListener { onItemSelected(layoutPosition) }
+
+        when (taskCategory) {
             TaskCategory.Business -> {
-                tvCategoryName.text = tvCategoryName.context.getString(R.string.todo_dialog_category_business)
-                divider.setBackgroundColor(ContextCompat.getColor(divider.context, R.color.todo_business_category))
+                categoryText = R.string.todo_dialog_category_business
+                categoryColor = R.color.todo_business_category
             }
+
             TaskCategory.Other -> {
-                tvCategoryName.text = tvCategoryName.context.getString(R.string.todo_dialog_category_other)
-                divider.setBackgroundColor(ContextCompat.getColor(divider.context, R.color.todo_other_category))
+                categoryText = R.string.todo_dialog_category_other
+                categoryColor = R.color.todo_other_category
             }
+
             TaskCategory.Personal -> {
-                tvCategoryName.text = tvCategoryName.context.getString(R.string.todo_dialog_category_personal)
-                divider.setBackgroundColor(ContextCompat.getColor(divider.context, R.color.todo_personal_category))
+                categoryText = R.string.todo_dialog_category_personal
+                categoryColor = R.color.todo_personal_category
             }
         }
+
+        tvCategoryName.text = tvCategoryName.context.getString(categoryText)
+        divider.setBackgroundColor(ContextCompat.getColor(divider.context, categoryColor))
     }
 }
