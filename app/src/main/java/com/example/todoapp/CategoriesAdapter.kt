562428fed18171433b39c526faca9724045797b4
Jose-Vela <joseeloyvo@gmail.com>
2023-06-10 02:18:24 -0600

    Creamos la lógica para filtrar las tareas por las categorias seleccionadas.
    - Cuando se selecciona la categoria (true), la card se muestra de un color en tonalidad azul marino (vuelve a su color por defecto) y automaticamente se muestran las tareas pertenecientes a esta categoria.
    - Cuando se deselecciona la categoria (false), se muestra la card de un color en tonalidad gris y automaticamente se ocultan las tareas pertenecientes a esta categoria.
    
    Nota: El valor por defecto (al iniciar la aplicación) de las categorias es "seleccionado" (true), mostrando todas las tareas que sean creadas por el usuario.

--- app/src/main/java/com/example/todoapp/CategoriesAdapter.kt
+++ app/src/main/java/com/example/todoapp/CategoriesAdapter.kt
@@ -4,7 +4,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class CategoriesAdapter(private val categories: List<TaskCategory>) :
+class CategoriesAdapter(
+    private val categories: List<TaskCategory>, private val onItemSelected: (Int) -> Unit) :
     RecyclerView.Adapter<CategoriesViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
         /*Este metodo crea una vista para "montarla" en pantalla, para que posteriormente el método
@@ -16,6 +17,10 @@ class CategoriesAdapter(private val categories: List<TaskCategory>) :
     override fun getItemCount() = categories.size   // Este metodo retorna el tamaño del listado de las categorías
 
     override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
-        holder.render(categories[position])
+        /* En este caso enviamos la función Lambda por parametro a la función render().
+         OJO: Se envia la función onItemSelected sin los parentesis (),
+         ya que de esta manera solo se envía, si colocamos los parentesis, es decir; onItemSelected(),
+         esta se ejecutaría   */
+        holder.render(categories[position], onItemSelected)
     }
 }
